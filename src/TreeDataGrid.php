47a23547463352d74b8d1a2260b75865bfb5a0a7
nece001 <nece001@163.com>
2026-05-24 18:20:54 +0800

    加自定义字段

--- src/TreeDataGrid.php
+++ src/TreeDataGrid.php
@@ -4,8 +4,15 @@ namespace Nece\WebUi;
 
 class TreeDataGrid extends DataGrid
 {
+    public function setAsync(bool $async = true): static
+    {
+        $this->config['async'] = $async;
+        return $this;
+    }
+
     public function setAsyncUrl(string $url): static
     {
+        $this->setAsync(true);
         $this->config['async_url'] = $url;
         return $this;
     }
@@ -21,13 +28,44 @@ class TreeDataGrid extends DataGrid
      */
     public function setAsyncParams(array $params): static
     {
+        $this->setAsync(true);
         $this->config['async_params'] = $params;
         return $this;
     }
 
-    public function setPagination(bool $pagination = true): static
+    public function setCustomChildrenField(string $custom_children_field): static
     {
-        $this->config['pagination'] = $pagination;
+        $this->config['custom_children_field'] = $custom_children_field;
+        return $this;
+    }
+
+    public function setCustomIsParentField(string $custom_is_parent_field): static
+    {
+        $this->config['custom_is_parent_field'] = $custom_is_parent_field;
+        return $this;
+    }
+
+    public function setCustomNameField(string $custom_name_field): static
+    {
+        $this->config['custom_name_field'] = $custom_name_field;
+        return $this;
+    }
+
+    public function setCustomIdField(string $custom_id_field): static
+    {
+        $this->config['custom_id_field'] = $custom_id_field;
+        return $this;
+    }
+
+    public function setCustomPidField(string $custom_pid_field): static
+    {
+        $this->config['custom_pid_field'] = $custom_pid_field;
+        return $this;
+    }
+
+    public function setCustomIconField(string $custom_icon_field): static
+    {
+        $this->config['custom_icon_field'] = $custom_icon_field;
         return $this;
     }
 }
