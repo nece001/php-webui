75dbfc09d03e182a1f1b0253435b628b604b21d1
nece001 <nece001@163.com>
2026-05-24 19:58:39 +0800

    加注释

--- src/TreeDataGrid.php
+++ src/TreeDataGrid.php
@@ -33,36 +33,90 @@ class TreeDataGrid extends DataGrid
         return $this;
     }
 
+    /**
+     * 设置包含子项的字段名
+     *
+     * @author nece001@163.com
+     * @create 2026-05-24 19:43:09
+     *
+     * @param string $custom_children_field
+     * @return static
+     */
     public function setCustomChildrenField(string $custom_children_field): static
     {
         $this->config['custom_children_field'] = $custom_children_field;
         return $this;
     }
 
+    /**
+     * 设置是否为父级的字段名
+     *
+     * @author nece001@163.com
+     * @create 2026-05-24 19:43:26
+     *
+     * @param string $custom_is_parent_field
+     * @return static
+     */
     public function setCustomIsParentField(string $custom_is_parent_field): static
     {
         $this->config['custom_is_parent_field'] = $custom_is_parent_field;
         return $this;
     }
 
+    /**
+     * 设置显示树形结构的字段名
+     *
+     * @author nece001@163.com
+     * @create 2026-05-24 19:43:51
+     *
+     * @param string $custom_name_field
+     * @return static
+     */
     public function setCustomNameField(string $custom_name_field): static
     {
         $this->config['custom_name_field'] = $custom_name_field;
         return $this;
     }
 
+    /**
+     * 设置ID字段名
+     *
+     * @author nece001@163.com
+     * @create 2026-05-24 19:44:35
+     *
+     * @param string $custom_id_field
+     * @return static
+     */
     public function setCustomIdField(string $custom_id_field): static
     {
         $this->config['custom_id_field'] = $custom_id_field;
         return $this;
     }
 
+    /**
+     * 设置父级ID字段名
+     *
+     * @author nece001@163.com
+     * @create 2026-05-24 19:44:41
+     *
+     * @param string $custom_pid_field
+     * @return static
+     */
     public function setCustomPidField(string $custom_pid_field): static
     {
         $this->config['custom_pid_field'] = $custom_pid_field;
         return $this;
     }
 
+    /**
+     * 设置图标字段名
+     *
+     * @author nece001@163.com
+     * @create 2026-05-24 19:44:48
+     *
+     * @param string $custom_icon_field
+     * @return static
+     */
     public function setCustomIconField(string $custom_icon_field): static
     {
         $this->config['custom_icon_field'] = $custom_icon_field;
