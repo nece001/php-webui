3e96355b29039e12afb983d9aeb8a1d3356969a3
nece001 <nece001@163.com>
2026-05-28 17:51:51 +0800

    添加打开窗口标记

--- src/IconList.php
+++ src/IconList.php
@@ -9,4 +9,10 @@ class IconList extends Component
         $this->config['bind_id'] = $bind_id;
         return $this;
     }
+
+    public function setIsOpenWindow(bool $is_open_window = true): static
+    {
+        $this->config['is_open_window'] = $is_open_window;
+        return $this;
+    }
 }
